82fe42042035f27774b9605cb05d4e0a8c3f9a0b
taku00 <takumioka428@gmail.com>
2024-05-04 11:22:24 +0900

    add options

--- add_chat_vector.py
+++ add_chat_vector.py
@@ -1,15 +1,24 @@
 import argparse
 
 import torch  # torch_dtypeで必要
+import torch.nn.functional as F
 from huggingface_hub import create_repo
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-b", "--base_model", type=str, required=True)
-parser.add_argument("-c", "--chat_model", type=str, required=True)
-parser.add_argument("-o", "--original_model", type=str, required=True)
-parser.add_argument("-t", "--dtype", type=str, default="torch.bfloat16")
-parser.add_argument("-f", "--use_flash_attention2", action="store_true", default=False)
+parser.add_argument("-bm", "--base_model", type=str, required=True)
+parser.add_argument("-cm", "--chat_model", type=str, required=True)
+parser.add_argument("-om", "--original_model", type=str, required=True)
+parser.add_argument("-dt", "--dtype", type=str, default="torch.bfloat16")
+parser.add_argument(
+    "-ufa", "--use_flash_attention2", action="store_true", default=False
+)
+parser.add_argument("-sl", "--skip_layernorm", action="store_true", default=False)
+parser.add_argument(
+    "-sel", "-skip_embedding_lmhead", action="store_true", default=False
+)
+parser.add_argument("-c", "--coefficient", type=float, default=1)
+parser.add_argument("-debug", "--debug", action="store_true", default=False)
 args = parser.parse_args()
 
 if not "torch." in args.dtype:
@@ -49,15 +58,26 @@ original_model = AutoModelForCausalLM.from_pretrained(
 skip_parameters = ["model.embed_tokens.weight", "lm_head.weight"]
 
 for k, v in original_model.state_dict().items():
-    if k in skip_parameters:
+    if args.skip_embedding_lmhead and k in skip_parameters:
+        continue
+    if args.skip_layernorm and "layernorm" in k:
         continue
     chat_vector = chat_model.state_dict()[k] - base_model.state_dict()[k]
-    v.copy_(v + chat_vector)
+    if k in skip_parameters:
+        chat_vector = F.pad(chat_vector, (0, 0, 0, v.size()[0] - chat_vector.size()[0]))
+    v.copy_(v + chat_vector * args.coefficient)
 
-original_model.save_pretrained("new_model")
-tokenizer.save_pretrained("new_model")
+if args.debug:
+    original_model.save_pretrained("new_model")
+    tokenizer.save_pretrained("new_model")
 
 repo_name = f"{args.original_model}_add-chat-vector"
+if not args.skip_embedding_lmhead:
+    repo_name += "-including-EM-LM"
+if args.skip_layernorm:
+    repo_name += "-except-LN"
+if args.coefficient != 1:
+    repo_name += f"-{args.coefficient}"
 try:
     create_repo(repo_name, repo_type="model", private=True)
 except Exception as e:
